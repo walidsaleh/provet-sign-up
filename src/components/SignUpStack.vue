c73a2197161b46d634ecf6e941a089019e735286
Walid Saleh Sánchez <superwal@gmail.com>
2025-01-30 00:16:38 +0100

    create container with some styles

--- src/components/SignUpStack.vue
+++ src/components/SignUpStack.vue
@@ -1,2 +1,21 @@
 <script setup lang="ts">
 </script>
+
+<template>
+  <provet-stack
+    class="sign-up-stack"
+    direction="vertical"
+    align-items="center"
+    justify-content="center"
+  >
+  </provet-stack>
+</template>
+
+<style scoped>
+.sign-up-stack {
+  inline-size: 90%;
+  max-inline-size: 600px;
+  margin: var(--n-space-xl) auto;
+  row-gap: var(--n-space-xl);
+}
+</style>
