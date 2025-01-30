7e28589828ce35a4a7e43c004bd92e2bee5def1c
Walid Saleh Sánchez <superwal@gmail.com>
2025-01-30 21:30:59 +0100

    feat: Add toast feedback to inform users about the status of their actions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import SignUpStack from '@/components/SignUpStack.vue'
+import type { ToastMessage } from '@/types/toast'
+
+const emptyToastMessage: ToastMessage = { message: '', variant: 'default' }
+const toastMessage = ref<ToastMessage>(emptyToastMessage)
+
+const hideToast = () => (toastMessage.value = emptyToastMessage)
+const displayToast = (toastMessageObject: ToastMessage) => (toastMessage.value = toastMessageObject)
 </script>
 
 <template>
-  <SignUpStack />
+  <SignUpStack @toast-requested="displayToast" />
+  <provet-toast-group @dismiss="hideToast">
+    <provet-toast
+      v-if="toastMessage.message"
+      :key="toastMessage.message"
+      :variant="toastMessage.variant"
+    >
+      {{ toastMessage.message }}
+    </provet-toast>
+  </provet-toast-group>
 </template>
